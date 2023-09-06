45b7cbbb453e71aebd137150318baa88ae042d38
Rohan Khayech <rohankhayech@gmail.com>
2023-09-06 17:23:39 +0800

    Added Dokka plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,9 @@ plugins {
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.android.library") version "8.1.0" apply false
+    id("org.jetbrains.dokka") version "1.9.0"
+}
+
+subprojects {
+    apply(plugin = "org.jetbrains.dokka")
 }
